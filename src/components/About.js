b14e1c18d9305c6a70bd0ca8766668b480f9c600
John John Penafiel <johnjohnpenafiel@hotmail.com>
2024-05-20 18:22:45 -0400

    Props Basics Lab

--- src/components/About.js
+++ src/components/About.js
@@ -1,14 +1,21 @@
 import React from "react";
+import Links from "./Links.js"
 
-function About() {
+function About(props) {
+  console.log(props)
+  
+  const biography = !props.bio ? null : <p>{props.bio}</p>
+
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>Put the bio in here</p>
+      {biography}
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-      {/* add your <Links /> component here */}
+      <Links github={props.github} linkedin={props.linkedin}/>
     </div>
   );
 }
 
 export default About;
+
+
